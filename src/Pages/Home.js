b1caf8da82a0f7980cec18c2f41f59099ec199be
pritam0710 <prakashpritam15@gmail.com>
2021-09-22 18:18:22 +0530

    Created search and fetch results from the tv maze api

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MiniPageLayout from '../components/MiniPageLayout';
 
 const Home = () => {
+  const [input, setInput] = useState('');
+
+  const onInputChange = evt => {
+    setInput(evt.target.value);
+  };
+
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(res => res.json())
+      .then(data => console.log(data));
+  };
+
+  const onKeyDown = evt => {
+    if (evt.keyCode === 13) {
+      onSearch();
+    }
+  };
+
   return (
     <MiniPageLayout>
-      <h1>Home Page!</h1>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="submit" onClick={onSearch}>
+        Search
+      </button>
     </MiniPageLayout>
   );
 };
